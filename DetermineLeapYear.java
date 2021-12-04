9ff6e2092bdd4f83c04ed062effe3054e84a9b15
Inayat Ullah Wani <73710823+Inayatwani@users.noreply.github.com>
2021-12-04 12:26:47 +0530

    Add files via upload

---
+++ DetermineLeapYear.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class DetermineLeapYear {
+
+    public class LeapYear {
+        public static void main(String[] args) {
+            int year;
+            System.out.println("Enter an Year :: ");
+            Scanner sc = new Scanner(System.in);
+            year = sc.nextInt();
+
+            if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+                System.out.println("year is a leap year");
+            else
+                System.out.println(" year is not a leap year");
+        }
+    }
+}
+
